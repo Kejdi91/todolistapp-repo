7975192c266a3254774889ae0c609bab5fc1d5d9
Kejdi Nela <153378077+Kejdi91@users.noreply.github.com>
2024-08-11 18:06:30 +0200

    to do list app code

---
+++ to-do-list-app/src/App.jsx
@@ -0,0 +1,8 @@
+import ToDoList1 from "./ToDoList1.jsx";
+
+function App(){
+return (<ToDoList1/>)
+
+}
+
+export default App;
---
+++ to-do-list-app/src/ToDoList1.jsx
@@ -0,0 +1,97 @@
+import React,{useState}  from "react"
+
+function ToDoList1(){
+
+    const [tasks,setTasks]=useState([]);
+    const [newTask,setNewTask]=useState("");
+
+    function HandleInputChange(event){
+        setNewTask(event.target.value);
+
+    }
+
+    function addTask(){
+
+        if(newTask.trim() !== ""){
+            setTasks(t => [...t,newTask]);
+            setNewTask("");
+        }
+
+    }
+
+    function deleteTask(index){
+        const updateTasks = tasks.filter((_,i)=> i !== index);
+        setTasks(updateTasks);
+    }
+
+    function moveTaskUp(index){
+        if(index > 0){
+            const updateTasks=[...tasks];
+        [updateTasks[index],updateTasks[index-1]]=
+        [updateTasks[index-1],updateTasks[index]];
+        setTasks(updateTasks);
+        }
+
+    }
+    
+    function moveTaskDown(index){
+        if(index < tasks.length -1 ){
+            const updateTasks=[...tasks];
+        [updateTasks[index],updateTasks[index+1]]=
+        [updateTasks[index+1],updateTasks[index]];
+        setTasks(updateTasks);
+        }
+
+    }
+
+    
+    return(
+        <div className="to-do-list">
+            <h1>To Do List</h1>
+
+            <div>
+                <input
+                type="text"
+                placeholder="Enter a task..."
+                value={newTask}
+                onChange={HandleInputChange}
+                />
+                <button
+                className="add-button"
+                onClick={addTask}>
+                    Add
+
+
+                </button>
+            </div>
+            <ol>
+                {tasks.map((task,index)=>
+                <li key={index}>
+                    <spa className="text">{task}</spa>
+                    <button
+                    className="delete-button"
+                    onClick={()=> deleteTask(index)}>
+                        Delete
+                    </button>
+                    <button
+                    className="move-button"
+                    onClick={()=> moveTaskUp(index)}>
+                       Up
+                    </button>
+                    <button
+                    className="move-button"
+                    onClick={()=> moveTaskDown(index)}>
+                       Down
+                    </button>
+                </li>)}
+            </ol>
+
+
+        </div>);
+    
+
+}
+
+
+
+export default ToDoList1
